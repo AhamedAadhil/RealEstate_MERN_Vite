32768b865486a4d48218dea239ffe4b76d13b99c
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-22 23:15:37 +0530

    database connection established
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server is Running on port 3000`);
+/* CONNECT DATABASE */
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Database Connected!"))
+  .catch((error) => console.log(error.message));
+
+/* APP START */
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is Running on port ${PORT}`);
 });
