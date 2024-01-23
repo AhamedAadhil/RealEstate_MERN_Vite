9ff539345bd6667b5e0c27eb1e77010d558ab752
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-23 22:37:18 +0530

    redux persist added
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Toaster position="top-center" reverseOrder={false} />
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
