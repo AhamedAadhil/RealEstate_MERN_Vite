7c1ea54b7217e4594f8015f96a58e025d6b22ace
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-23 22:18:44 +0530

    redux store created
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Toaster position="top-center" reverseOrder={false} />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInFailure,
+  signInSuccess,
+} from "../redux/user/userSlice";
 import toast from "react-hot-toast";
 
 export const SignIn = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [formdata, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
+  const { loading } = useSelector((state) => state.user);
 
   const handleChange = (e) => {
     setFormData({
@@ -17,7 +24,7 @@ export const SignIn = () => {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      setLoading(true);
+      dispatch(signInStart());
       const response = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,15 +34,15 @@ export const SignIn = () => {
       });
       const data = await response.json();
       if (data.success === false) {
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         toast.error(data.message);
         return;
       }
       toast.success("Logged In!");
-      setLoading(false);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
+      dispatch(signInFailure(error.message));
       toast.error(error.message);
     }
   };
