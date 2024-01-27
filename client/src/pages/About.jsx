f87052ceb6440f6802580e89c42eabead0a48048
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-27 22:50:13 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,26 @@
-import React from "react";
-
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Aathil Builders
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Hi There I am Ahamed Aathil Junain, From Srilanka. I have created this
+        whole web application with the help of my own knowledge, many YouTubers
+        and ChatGPT. First of all I would like to extend my thanks to them.
+        Cheers mate..
+      </p>
+      <p className="mb-4 text-slate-700">
+        To do this website I took nearly just 5 days, even I follow the video in
+        Youtube which was 11 hours length. I didnt belive I did complete this
+        web application this much of fast, normally I spent 2 to 3 hours a day
+        for this project to complete it this fast.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Hope everyone will enjoy this project if someone clone this code from my
+        github repository, for any further doubts and query please dont hesitate
+        to contact me through Ahamedaathil.5@gmail.com
+      </p>
+    </div>
+  );
 }
