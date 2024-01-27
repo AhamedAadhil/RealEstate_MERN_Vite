adf5243e449e8b24e0aca66beb9d44c88d0f6db6
Ahamed Aathil <ahamedaathil.5@gmail.com>
2024-01-27 22:41:02 +0530

    complete homepage
    Signed-off-by: Ahamed Aathil <ahamedaathil.5@gmail.com>

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,155 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import ListingCard from "../components/ListingCard";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  SwiperCore.use(Navigation);
+
+  useEffect(() => {
+    const fetchOfferListings = async () => {
+      try {
+        const response = await fetch(`/api/listing/get?offer=true&limit=4`);
+        const data = await response.json();
+        setOfferListings(data);
+        fetchRentListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchRentListings = async () => {
+      try {
+        const response = await fetch(`/api/listing/get?type=rent&limit=4`);
+        const data = await response.json();
+        setRentListings(data);
+        fetchSaleListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchSaleListings = async () => {
+      try {
+        const response = await fetch(`/api/listing/get?type=sale&limit=4`);
+        const data = await response.json();
+        setSaleListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchOfferListings();
+  }, []);
+  return (
+    <div>
+      {/* top */}
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span> <br />
+          place with ease
+        </h1>
+        <div className="text-gray-400 text-xs sm:text-sm">
+          Aathil Builders is one of the leading Real Estate businessers having
+          huge reputiton and trust from people. <br /> We have list of
+          properties to choose from.
+        </div>
+        <Link
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
+          to={"/search"}
+        >
+          Lets find the best..
+        </Link>
+      </div>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <SwiperSlide>
+              <img
+                src={listing.imageUrls[0]}
+                alt={`Slide ${index}`}
+                className="w-full h-auto object-cover"
+                style={{ maxHeight: "550px", backgroundSize: "cover" }}
+              />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results */}
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Offers
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?offer=true`}
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {rentListings && rentListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent places for Rent
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?type=rent`}
+              >
+                Show more places for Rent
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {rentListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {saleListings && saleListings.length > 0 && (
+          <div>
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Sales
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={`/search?type=sale`}
+              >
+                Show more Sales
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {saleListings.map((listing) => (
+                <ListingCard listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
